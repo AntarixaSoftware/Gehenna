7b4a5906d2ca421298a4f3d753a749d3fc39814d
R-Reeve <168000783+R-Reeve@users.noreply.github.com>
2024-05-01 12:14:19 +0700

    Add files via upload

---
+++ Code_Gehenna.py
@@ -0,0 +1,56 @@
+import sys
+
+import pygame
+
+class Game :
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('Tubes PBO')
+        self.screen = pygame.display.set_mode((640, 480))
+
+        self.clock = pygame.time.Clock()
+
+        self.img = pygame.image.load('Data/images/clouds/cloud_1.png')
+        self.img.set_colorkey((0, 0, 0))
+
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
+
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
+
+
+    def run(self):
+        while True:
+            self.screen.fill((14, 219, 248))
+
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            self.screen.blit(self.img, self.img_pos)
+
+            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
+            else:
+                pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
+
+
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        self.movement[0] = True
+                    if event.key == pygame.K_s:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w:
+                        self.movement[0] = False
+                    if event.key == pygame.K_s:
+                        self.movement[1] = False
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
